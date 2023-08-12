2c2697b104fc9fc952da1db895e5f474d60c4aa4
dylviz <dylanvizcarra@gmail.com>
2023-08-11 23:20:57 -0700

    debuggin api

--- app/api/read-write-data-table/route.ts
+++ app/api/read-write-data-table/route.ts
@@ -1,7 +1,31 @@
 import { sql } from "@vercel/postgres";
 import { NextApiRequest, NextApiResponse } from "next";
+import Cors from "cors";
+
+const cors = Cors({
+	methods: ["POST", "GET", "HEAD"],
+});
+
+function runMiddleware(
+	req: NextApiRequest,
+	res: NextApiResponse,
+	fn: Function
+) {
+	return new Promise((resolve, reject) => {
+		fn(req, res, (result: any) => {
+			if (result instanceof Error) {
+				return reject(result);
+			}
+
+			return resolve(result);
+		});
+	});
+}
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
+	// Run the middleware
+	await runMiddleware(req, res, cors);
+
 	const { method } = req;
 
 	switch (method) {
