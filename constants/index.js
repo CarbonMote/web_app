79aac2e2c107c9db349b0d1a5039e4b6ddab5b88
dylviz <dylanvizcarra@gmail.com>
2023-08-13 05:01:01 -0700

    update

--- constants/index.js
+++
@@ -1,4 +0,0 @@
-export const contractAddresses = require("./contract_addresses");
-export const contractAbi = require("./contract_abi");
-export const connectedHandler = data => console.log("CONNECTED", data);
-export const disconnectedHandler = data => console.log("DISCONNECTED", data);
